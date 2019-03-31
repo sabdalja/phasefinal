825aa69b02a4a98a9ff10d54d68ff0a1edef740e
Samir Abdaljalil <sabdalja@andrew.cmu.edu>
2019-03-31 09:16:37 +0000

    Fixed views and added webmaster

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,19 @@
 // about supported directives.
 //
 //= require rails-ujs
-//= require activestorage
-//= require turbolinks
+//= require jquery
+//= require materialize-sprockets 
 //= require_tree .
+
+$( document ).ready(function () {
+    $(".dropdown-button").dropdown();
+    $('select').material_select();
+    $('.datepicker').pickadate({
+        format: 'mmmmdd,yyyy',
+        formatSubmit: 'mmmmdd,yyyy',
+        selectMonths: true, // Creates a dropdown to control month selectYears: 15, // Creates a dropdown of 15 years to control year, today: 'Today',
+        clear: 'Clear',
+        close: 'Ok',
+        closeOnSelect: false // Close upon selecting a date,
+    });
+})
